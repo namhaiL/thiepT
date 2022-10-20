c05d8e20e0b23f3118793e5433246b9c3c20f983
haialibaba <94033066+haialibaba@users.noreply.github.com>
2022-10-20 17:05:32 +0700

    commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc bé của Hải 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bé trong hôm nay và cả những ngày sau. Mãi iu <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Thanh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
